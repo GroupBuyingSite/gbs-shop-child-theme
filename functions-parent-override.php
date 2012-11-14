0b340b0c28c59ec517bbf1e758ee28e149e9ca05
Dan Cameron <dan@sproutventure.com>
2012-11-14 11:11:31 -0800

    Redirect from homepage compatibility.

--- functions-parent-override.php
+++ functions-parent-override.php
@@ -5,3 +5,33 @@
  *
  *
  */
+
+
+/**
+ * Redirect to the latest deal
+ *
+ * @return null redirect
+ */
+function gb_redirect_from_home() {
+
+	if ( !is_user_logged_in() && gb_force_login_option() != 'false' ) {
+		if (
+			( is_home() && 'subscriptions' == gb_force_login_option() ) ||
+			gb_on_login_page() ||
+			gb_on_reset_password_page() ) {
+			return;
+		} else {
+			gb_set_message( gb__( 'Force Login Activated, Membership Required.' ) );
+			gb_login_required();
+			return;
+		}
+	}
+
+	if ( is_home() ) {
+		if ( is_user_logged_in() || gb_has_location_preference() ) { // redirect for logged in users
+			wp_redirect( apply_filters( 'gb_latest_deal_redirect', gb_get_latest_deal_link() ) );
+			exit();
+		}
+	}
+}
+add_action( 'pre_gbs_head', 'gb_redirect_from_home' );
